4356073ce3f27445e14dc5bd88fa4e3188055bc0
tallminion <sagarkr1109@gmail.com>
2024-10-05 17:57:47 +0530

    JWTRequest and JWTResponse to receive request data and send Login success response

---
+++ src/main/java/com/minion/jwt/models/JWTResponse.java
@@ -0,0 +1,14 @@
+package com.minion.jwt.models;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class JWTResponse {
+    private String jwtToken;
+    private String username;
+}
